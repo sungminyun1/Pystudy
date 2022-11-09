525902067529e4ad98f76d7ab6875c4448ce106f
unknown <smy07207@gmail.com>
2022-11-09 23:37:06 +0900

    1109

--- pythonicCode.py
+++ pythonicCode.py
@@ -17,3 +17,22 @@ words = 'The quick brown fox jumps over the lazy dog'.split()
 result = [[i.upper(),i.lower(),len(i)]for i in words]
 
 print(result)
+
+alist = ['a1','a2','a3']
+blist = ['b1','b2','b3']
+
+for a,b in zip(alist,blist):
+    print(a,b)
+
+
+alist = ['a1', 'a2', 'a3']
+blist = ['b1', 'b2', 'b3']
+
+for i, (a, b) in enumerate(zip(alist, blist)):
+    print(i, a, b)
+
+f= lambda x,y: x+y
+print(f(1,4))
+
+ex = [1,2,3,4,5]
+print(list(map(lambda x: x **2 if x %2 ==0 else x, ex)))
