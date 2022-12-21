1e375d9f7c6eb7191572e37e347fc675662d8d03
unknown <smy07207@gmail.com>
2022-12-21 23:11:23 +0900

    1221

--- my/test.py
+++ my/test.py
@@ -1,1 +1,3 @@
 #https://wooody92.github.io/os/%EB%A9%80%ED%8B%B0-%ED%94%84%EB%A1%9C%EC%84%B8%EC%8A%A4%EC%99%80-%EB%A9%80%ED%8B%B0-%EC%8A%A4%EB%A0%88%EB%93%9C/
+
+#https://docs.python.org/ko/3/library/multiprocessing.html
